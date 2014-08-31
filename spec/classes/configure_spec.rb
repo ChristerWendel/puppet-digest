e1c43d60510ac45b1aa0b6bd90930109dffda137
Christer Wendel <christer.wendel@altrusoft.se>
2014-08-31 14:23:35 +0200

    Updated tests

--- spec/classes/configure_spec.rb
+++ spec/classes/configure_spec.rb
@@ -5,9 +5,17 @@ describe 'digest', :type => :class do
 
   it { should contain_file('/etc/apache2/htpasswd')
     .with_ensure('present')
+    .with_owner('www-data')
+    .with_group('www-data')
+    .with_mode('0600')
+    .that_comes_before('Apache::loadmodule[auth_digest]')
   }
 
   it { should contain_file('/etc/apache2/conf-available/digest.conf')
     .with_ensure('present')
+    .with_owner('www-data')
+    .with_group('www-data')
+    .with_mode('0644')
+    .that_comes_before('Apache::loadconf[digest]')
   }
 end
